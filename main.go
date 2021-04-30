b6c56a5f0543206155a833779802ff5e7e53899a
Anthony Pearce <tonytwo98@gmail.com>
2021-04-30 15:21:17 -0500

    Added Multi-Pendulum Support
    Split the file as to have the calculations done in a separate location. And allowed support for multiple pendulums.

--- main.go
+++ main.go
@@ -2,25 +2,266 @@ package main
 
 import (
 	"image/color"
-	"math"
 
 	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 )
 
-var (
-	grav           = 1.0
-	radius1        = 300.0
-	radius2        = 400.0
-	mass1          = 64.0
-	mass2          = 64.0
-	thetaVel1      = 0.0
-	thetaVel2      = 0.0
-	theta1, theta2 = a1a2DefaultValues()
-)
-
 func run() {
+	var pendulums []sPendulum
+	pendulums = append(pendulums, sPendulum{
+		grav:      1.5,
+		radius1:   400.0,
+		radius2:   400.0,
+		mass1:     64.0,
+		mass2:     64.0,
+		thetaVel1: 0.0,
+		thetaVel2: 0.0,
+		theta1:    3.1415 / 1.2,
+		theta2:    3.1415 / 2.0,
+		color:     color.NRGBA{255, 0, 0, 255},
+	},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1414 / 1.2,
+			theta2:    3.1414 / 2.0,
+			color:     color.NRGBA{255, 0, 50, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1413 / 1.2,
+			theta2:    3.1413 / 2.0,
+			color:     color.NRGBA{255, 0, 100, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1412 / 1.2,
+			theta2:    3.1412 / 2.0,
+			color:     color.NRGBA{255, 0, 150, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1411 / 1.2,
+			theta2:    3.1411 / 2.0,
+			color:     color.NRGBA{255, 0, 200, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1410 / 1.2,
+			theta2:    3.1410 / 2.0,
+			color:     color.NRGBA{255, 0, 250, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1409 / 1.2,
+			theta2:    3.1409 / 2.0,
+			color:     color.NRGBA{200, 0, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1408 / 1.2,
+			theta2:    3.1408 / 2.0,
+			color:     color.NRGBA{150, 0, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1407 / 1.2,
+			theta2:    3.1407 / 2.0,
+			color:     color.NRGBA{100, 0, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1406 / 1.2,
+			theta2:    3.1406 / 2.0,
+			color:     color.NRGBA{50, 0, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1405 / 1.2,
+			theta2:    3.1405 / 2.0,
+			color:     color.NRGBA{0, 0, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1404 / 1.2,
+			theta2:    3.1404 / 2.0,
+			color:     color.NRGBA{0, 50, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1403 / 1.2,
+			theta2:    3.1403 / 2.0,
+			color:     color.NRGBA{0, 100, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1402 / 1.2,
+			theta2:    3.1402 / 2.0,
+			color:     color.NRGBA{0, 150, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1401 / 1.2,
+			theta2:    3.1401 / 2.0,
+			color:     color.NRGBA{0, 200, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1400 / 1.2,
+			theta2:    3.1400 / 2.0,
+			color:     color.NRGBA{0, 255, 255, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1399 / 1.2,
+			theta2:    3.1399 / 2.0,
+			color:     color.NRGBA{0, 255, 200, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1398 / 1.2,
+			theta2:    3.1398 / 2.0,
+			color:     color.NRGBA{0, 255, 150, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1397 / 1.2,
+			theta2:    3.1397 / 2.0,
+			color:     color.NRGBA{0, 255, 100, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1396 / 1.2,
+			theta2:    3.1396 / 2.0,
+			color:     color.NRGBA{0, 255, 50, 255},
+		},
+		sPendulum{
+			grav:      1.5,
+			radius1:   400.0,
+			radius2:   400.0,
+			mass1:     64.0,
+			mass2:     64.0,
+			thetaVel1: 0.0,
+			thetaVel2: 0.0,
+			theta1:    3.1395 / 1.2,
+			theta2:    3.1395 / 2.0,
+			color:     color.NRGBA{0, 255, 0, 255},
+		})
+
 	winWidth := 1080 * 2
 	winHeight := 786 * 2
 	win, err := pixelgl.NewWindow(pixelgl.WindowConfig{
@@ -38,76 +279,14 @@ func run() {
 	for !win.Closed() {
 		win.SetClosed(win.JustPressed(pixelgl.KeyEscape) || win.JustPressed(pixelgl.KeyQ))
 
-		if win.JustPressed(pixelgl.KeySpace) {
-			theta1, theta2 = a1a2DefaultValues()
-		}
-
 		win.Clear(color.NRGBA{44, 44, 84, 255})
-
-		a, b := update()
-
-		imd := imdraw.New(nil)
-
-		imd.Color = color.NRGBA{64, 64, 122, 255}
-		imd.Push(pixel.ZV, a, b)
-		imd.Line(3)
-
-		imd.Color = color.NRGBA{51, 217, 178, 255}
-		imd.Push(a)
-		imd.Circle(mass1/2, 0)
-
-		imd.Color = color.NRGBA{52, 172, 224, 255}
-		imd.Push(b)
-		imd.Circle(mass2/2, 0)
-
-		imd.Draw(win)
+		for i := 0; i < len(pendulums); i++ {
+			pendulums[i].draw(win)
+		}
 		win.Update()
 	}
 }
 
-func update() (pixel.Vec, pixel.Vec) {
-	a1a := a1aCalculation()
-	a2a := a2aCalculation()
-
-	thetaVel1 += a1a
-	thetaVel2 += a2a
-
-	theta1 += thetaVel1
-	theta2 += thetaVel2
-
-	thetaVel1 *= 0.9996
-	thetaVel2 *= 0.9996
-
-	a := pixel.V(radius1*math.Sin(theta1), radius1*math.Cos(theta1))
-	b := pixel.V(a.X+radius2*math.Sin(theta2), a.Y+radius2*math.Cos(theta2))
-
-	return a, b
-}
-
 func main() {
 	pixelgl.Run(run)
-}
-
-func a1a2DefaultValues() (float64, float64) {
-	return math.Pi / 2, math.Pi / 2
-}
-
-func a1aCalculation() float64 {
-	num1 := -grav * (2*mass1 + mass2) * math.Sin(theta1)
-	num2 := -mass2 * grav * math.Sin(theta1-2*theta2)
-	num3 := -2 * math.Sin(theta1-theta2) * mass2
-	num4 := thetaVel2*thetaVel2*radius2 + thetaVel1*thetaVel1*radius1*math.Cos(theta1-theta2)
-	den := radius1 * (2*mass1 + mass2 - mass2*math.Cos(2*theta1-2*theta2))
-
-	return (num1 + num2 + num3*num4) / den
-}
-
-func a2aCalculation() float64 {
-	num1 := 2 * math.Sin(theta1-theta2)
-	num2 := (thetaVel1 * thetaVel1 * radius1 * (mass1 + mass2))
-	num3 := grav * (mass1 + mass2) * math.Cos(theta1)
-	num4 := thetaVel2 * thetaVel2 * radius2 * mass2 * math.Cos(theta1-theta2)
-	den := radius2 * (2*mass1 + mass2 - mass2*math.Cos(2*theta1-2*theta2))
-
-	return (num1 * (num2 + num3 + num4)) / den
 }
---
+++ pendulum.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"image/color"
+	"math"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+)
+
+type sPendulum struct {
+	grav      float64
+	radius1   float64
+	radius2   float64
+	mass1     float64
+	mass2     float64
+	thetaVel1 float64
+	thetaVel2 float64
+	theta1    float64
+	theta2    float64
+	pendPos1  pixel.Vec
+	pendPos2  pixel.Vec
+	color     color.NRGBA
+}
+
+func (pendulum *sPendulum) update() {
+	pendulum.a1aCalculation()
+	pendulum.a2aCalculation()
+
+	pendulum.theta1 += pendulum.thetaVel1
+	pendulum.theta2 += pendulum.thetaVel2
+
+	pendulum.thetaVel1 *= 0.9996
+	pendulum.thetaVel2 *= 0.9996
+
+	pendulum.pendPos1 = pixel.V(pendulum.radius1*math.Sin(pendulum.theta1), pendulum.radius1*math.Cos(pendulum.theta1))
+	pendulum.pendPos2 = pixel.V(pendulum.pendPos1.X+pendulum.radius2*math.Sin(pendulum.theta2), pendulum.pendPos1.Y+pendulum.radius2*math.Cos(pendulum.theta2))
+}
+
+func (pendulum *sPendulum) a1aCalculation() {
+	num1 := -pendulum.grav * (2*pendulum.mass1 + pendulum.mass2) * math.Sin(pendulum.theta1)
+	num2 := -pendulum.mass2 * pendulum.grav * math.Sin(pendulum.theta1-2*pendulum.theta2)
+	num3 := -2 * math.Sin(pendulum.theta1-pendulum.theta2) * pendulum.mass2
+	num4 := pendulum.thetaVel2*pendulum.thetaVel2*pendulum.radius2 + pendulum.thetaVel1*pendulum.thetaVel1*pendulum.radius1*math.Cos(pendulum.theta1-pendulum.theta2)
+	den := pendulum.radius1 * (2*pendulum.mass1 + pendulum.mass2 - pendulum.mass2*math.Cos(2*pendulum.theta1-2*pendulum.theta2))
+
+	pendulum.thetaVel1 += (num1 + num2 + num3*num4) / den
+}
+
+func (pendulum *sPendulum) draw(win pixel.Target) {
+	pendulum.update()
+
+	imd := imdraw.New(nil)
+
+	imd.Color = color.NRGBA{0, 0, 0, 255}
+	imd.Push(pixel.ZV, pendulum.pendPos1, pendulum.pendPos2)
+	imd.Line(3)
+
+	imd.Color = pendulum.color
+	imd.Push(pendulum.pendPos1)
+	imd.Circle(pendulum.mass1/2, 0)
+
+	imd.Color = pendulum.color
+	imd.Push(pendulum.pendPos2)
+	imd.Circle(pendulum.mass2/2, 0)
+
+	imd.Draw(win)
+}
+
+func (pendulum *sPendulum) a2aCalculation() {
+	num1 := 2 * math.Sin(pendulum.theta1-pendulum.theta2)
+	num2 := (pendulum.thetaVel1 * pendulum.thetaVel1 * pendulum.radius1 * (pendulum.mass1 + pendulum.mass2))
+	num3 := pendulum.grav * (pendulum.mass1 + pendulum.mass2) * math.Cos(pendulum.theta1)
+	num4 := pendulum.thetaVel2 * pendulum.thetaVel2 * pendulum.radius2 * pendulum.mass2 * math.Cos(pendulum.theta1-pendulum.theta2)
+	den := pendulum.radius2 * (2*pendulum.mass1 + pendulum.mass2 - pendulum.mass2*math.Cos(2*pendulum.theta1-2*pendulum.theta2))
+
+	pendulum.thetaVel2 += (num1 * (num2 + num3 + num4)) / den
+}
